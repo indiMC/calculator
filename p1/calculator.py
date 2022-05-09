68c55358a383c48347ed74c9154f926b270656c2
indiMC <76277260+indiMC@users.noreply.github.com>
2022-05-09 22:36:45 +0530

    Add files via upload

---
+++ p1/calculator.py
@@ -0,0 +1,15 @@
+#first no
+fn = float(input("Enter your first number"))
+#second no
+sn = float(input("Enter your sencond number"))
+#operator eg:+,-,*,/.
+op = input("Enter the operator you want")
+
+if (op == '+'):
+    print(fn + sn)
+elif (op == '-'):
+    print(fn - sn)
+elif (op == '*'):
+    print(fn * sn)
+elif (op == '/'):
+    print(fn / sn)
